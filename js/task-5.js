652a1f293438c18f0942053cba0427f1a7ddd36b
IhorLev1 <lambermen1@gmail.com>
2020-07-07 00:08:16 +0300

    fixed

--- js/task-5.js
+++ js/task-5.js
@@ -4,34 +4,37 @@ const message = prompt("Укажите куда доставлять?");
 
 let country = "";
 let price = 0;
-
-switch (message.toUpperCase()) {
-  case "КИТАЙ":
-    country = "Китай";
-    price = 100;
-    break;
-  case "ЧИЛИ":
-    country = "Чили";
-    price = 250;
-    break;
-  case "АВСТРАЛИЯ":
-    country = "Австралия";
-    price = 170;
-    break;
-  case "ИНДИЯ":
-    country = "Индия";
-    price = 80;
-    break;
-  case "ЯМАЙКА":
-    country = "Ямайка";
-    price = 120;
-    break;
-  default:
-    price = 0;
-    break;
-}
-if (price === 0) {
-  alert("В вашей стране доставка не доступна");
+if (message === null) {
+  alert("Отменено пользователем");
 } else {
-  alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+  switch (message.toUpperCase()) {
+    case "КИТАЙ":
+      country = "Китай";
+      price = 100;
+      break;
+    case "ЧИЛИ":
+      country = "Чили";
+      price = 250;
+      break;
+    case "АВСТРАЛИЯ":
+      country = "Австралия";
+      price = 170;
+      break;
+    case "ИНДИЯ":
+      country = "Индия";
+      price = 80;
+      break;
+    case "ЯМАЙКА":
+      country = "Ямайка";
+      price = 120;
+      break;
+    default:
+      price = 0;
+      break;
+  }
+  if (price === 0) {
+    alert("В вашей стране доставка не доступна");
+  } else {
+    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+  }
 }
