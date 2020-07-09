7aceda9926c94e9e8b5e3bf09431e0a0366117db
IhorLev1 <lambermen1@gmail.com>
2020-07-09 17:11:44 +0300

    add Number.isNaN

--- js/task-4.js
+++ js/task-4.js
@@ -2,11 +2,13 @@
 
 const credits = 23580;
 const pricePerDroide = 3000;
-const message = prompt("Количество дроидов которые хотите купить?");
+const message = Number(prompt("Количество дроидов которые хотите купить?"));
 let totalPrice = message * pricePerDroide;
 
 if (message === null) {
   console.log("Отменено пользователем!");
+} else if (Number.isNaN(message)) {
+  alert("Вы ввели не число!");
 } else if (totalPrice > credits) {
   console.log("Недостаточно средств на счету!");
 } else {
